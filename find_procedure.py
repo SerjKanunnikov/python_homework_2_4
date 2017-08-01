50a2e3140b27a0ad739426363e169c003e8fe750
Kanunnikov Sergey <SergeyKanunnikov@MacBook-Pro-Kanunnikov.local>
2017-08-01 22:49:09 +0300

    Output

--- find_procedure.py
+++ find_procedure.py
@@ -3,21 +3,21 @@ import os
 
 
 def filter_list(files_list):
-    search_line = input("Вводи\n")
+    search_line = input("Введите слово для поиска:\n")
     for file in files_list:
         check_file = open(os.path.join(migrations, file), encoding="utf-8")
         if search_line in check_file.read():
             sql_list.append(file)
-    print(sql_list)
+            print(os.path.abspath(file))
     files_list = sql_list
-    print(files_list)
+    print("Всего:", len(sql_list))
+    # print(files_list)
     return files_list
+
 
 if __name__ == '__main__':
     migrations = os.path.join(os.getcwd(), "Migrations")
     files_list = [file for file in os.listdir(migrations) if file.endswith(".sql")]
-    # print(files_list)
     while True:
         sql_list = []
         files_list = filter_list(files_list)
-
