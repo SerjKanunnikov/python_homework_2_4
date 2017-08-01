a92d9df7a93ca3e59c2f19bca4e2fe063b509247
Sergey Kanunnikov <skanunnikov@restream.rt.ru>
2017-08-01 14:33:49 +0300

    logic fix

--- find_procedure.py
+++ find_procedure.py
@@ -1,30 +1,23 @@
 import os.path
 import os
 
-migrations = os.path.join(os.getcwd(), "Migrations")
-
-
-def find_files():
-    files_list = (file for file in os.listdir(migrations) if file.endswith(".sql"))
-    return files_list
-
 
 def filter_list(files_list):
-    sql_list = []
     search_line = input("Вводи\n")
     for file in files_list:
         check_file = open(os.path.join(migrations, file), encoding="utf-8")
         if search_line in check_file.read():
-            sql_list.append(os.path.join(migrations, file))
-    # new_list = sql_list
-    # for file in new_list:
-    #     check_file = open(os.path.join(migrations, file), encoding="utf-8")
-    #     if search_line in check_file.read():
-    #         sql_list.append(os.path.join(migrations, file))
+            sql_list.append(file)
     print(sql_list)
-
+    files_list = sql_list
+    print(files_list)
+    return files_list
 
 if __name__ == '__main__':
+    migrations = os.path.join(os.getcwd(), "Migrations")
+    files_list = [file for file in os.listdir(migrations) if file.endswith(".sql")]
+    # print(files_list)
     while True:
-        filter_list(find_files())
-pass
+        sql_list = []
+        files_list = filter_list(files_list)
+
