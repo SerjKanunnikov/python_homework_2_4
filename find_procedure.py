bbf67574a8214ddb4c2c03b803ec6ca9a4b47b92
Sergey Kanunnikov <skanunnikov@restream.rt.ru>
2017-09-13 13:47:04 +0200

    fix

--- find_procedure.py
+++ find_procedure.py
@@ -3,6 +3,7 @@ import os
 
 
 def filter_list(files_list):
+    sql_list = []
     search_line = input("Введите слово для поиска:\n")
     for file in files_list:
         check_file = open(os.path.join(migrations, file), encoding="utf-8")
@@ -11,13 +12,11 @@ def filter_list(files_list):
             print(os.path.abspath(file))
     files_list = sql_list
     print("Всего:", len(sql_list))
-    # print(files_list)
     return files_list
 
 
 if __name__ == '__main__':
-    migrations = os.path.join(os.getcwd(), "Migrations")
+    migrations = "Migrations"
     files_list = [file for file in os.listdir(migrations) if file.endswith(".sql")]
     while True:
-        sql_list = []
         files_list = filter_list(files_list)
